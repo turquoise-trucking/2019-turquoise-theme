f06f86b35951b1201660c7f94caccadfcc916b09
turquoise-trucking <57842675+turquoise-trucking@users.noreply.github.com>
2019-12-21 15:00:20 -0600

    Tutorial complete

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php get_header(); ?>
+<!-- WordPress loop -->
+<div>
+  <?php if ( have_posts() ) : while ( have_posts() ) :   the_post(); ?>
+    <h2>
+      <a href="<?php the_permalink() ?>">
+        <?php the_title(); ?>
+      </a>
+    </h2>
+    <?php the_content(); ?>
+  <?php endwhile; else: ?>
+    <p>There no posts to show</p>
+  <?php endif; ?>
+</div>
+<?php get_footer(); ?>
