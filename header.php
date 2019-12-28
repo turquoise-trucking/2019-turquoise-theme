70c985b8a7675a980cd084ab66b3d8eac4834445
turquoise-trucking <57842675+turquoise-trucking@users.noreply.github.com>
2019-12-28 15:41:45 -0600

    Update header.php

--- header.php
+++ header.php
@@ -1,42 +1,54 @@
 <!DOCTYPE html>
-<!--[if lt IE 7]><html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if IE 7]><html class="no-js lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]><html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
-  <head>
+<!--[if lt IE 7]><html lang="en" class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]><html lang="en" class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]><html lang="en" class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html lang="en" class="no-js"> <!--<![endif]-->
+<head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 
     <title><?php wp_title('&laquo;', true, 'right'); ?> <?php bloginfo('name'); ?></title>
 
     <meta name="description" content="">
-    <meta name="author" content="">
+    <meta name="author" content="John Earl Hardesty">
 
-    <meta name="viewport" content="width=device-width">
+    <!-- bootstrap styles -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" 
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" 
+        crossorigin="anonymous">
+    <!-- font awesome -->
+    <script src="https://kit.fontawesome.com/74abbf3163.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css">
+    <!-- favicon -->
+    <link rel="icon" type="image/png" sizes="16x16" href="/static-assets/tt-icon-16px.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/static-assets/tt-icon-32px.png">
+    <link rel="icon" type="image/png" sizes="64x64" href="/static-assets/tt-icon-64px.png">
+    <link rel="apple-touch-icon" type="image/png" href="/static-assets/tt-icon-apple.png">
+    <!-- local styles -->
+    <link rel="stylesheet" href="/static-assets/turquoise.css">
+
     <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
 
     <?php wp_head(); ?>
 </head>
+
 <body <?php body_class(); ?>>
-    <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-        <a class="navbar-brand" href="#">
-            <?php bloginfo('name'); ?>
-        </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <?php
-            wp_nav_menu([
-                'menu'            => 'primary',
-                'theme_location'  => 'menu-1',
-                'container'       => 'div',
-                'container_id'    => 'navbarCollapse',
-                'container_class' => 'collapse navbar-collapse',
-                'menu_id'         => false,
-                'menu_class'      => 'navbar-nav mr-auto',
-                'depth'           => 0,
-                'fallback_cb'     => 'bs4navwalker::fallback',
-                'walker'          => new bs4navwalker()
-            ]);
-        ?>
-    </nav>
+<!-- masthead -->
+<div class="masthead-wide masthead">
+    <div class="masthead-gradient">
+        <div class="container">
+            <div class="row ">
+                <div class="col-sm-6">
+                    <a href="index.html"><img class="logo" src="/static-assets/turquoise-logo.png" alt="turquoise trucking logo"></a>
+                </div>
+                <div class="col-sm-6 align-middle">
+                    <div class="btn-group float-right" style="padding: 0.75em;">
+                        <a class="btn btn-success" href="tel:+18447997900" role="button"><i class="fas fa-phone"></i>&nbsp;Call 844-799-7900</a>
+                        <a class="btn btn-primary" href="mailto:contact@turquoisetruck.com" role="button"><i class="fas fa-envelope"></i>&nbsp;Email&nbsp;us</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
