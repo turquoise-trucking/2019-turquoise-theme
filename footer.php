1cc879f8b3a4cc62602f2a82dd3ac5ffa5686c14
turquoise-trucking <57842675+turquoise-trucking@users.noreply.github.com>
2019-12-28 15:44:00 -0600

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,5 +1,76 @@
-    <footer>  
-    </footer>
-    <?php wp_footer(); ?>
-    </body>
+<!-- primary footer -->
+<div class="section footer-classic footer context-light bg-turquoise">
+    <div class="container">
+        <div class="row row-30">
+            <div class="col-md-4 col-xl-5">
+                <div class="pr-xl-4"><a class="brand" href="index.html"><img class="brand-logo-light" src="/static-assets/turquoise-badge.png" alt="" width="181" height="50"></a>
+                <p>With over 10 years of experience in the trucking business, we provide companies with safe, efficient, and reliable transportation solutions across the United States.</p>
+                <ul class="nav-list">
+                    <li class="nav-fb"><a href="https://www.facebook.com/TurquoiseTrucking/" target="_blank" rel="noreferrer" class="external-link"><i class="fab fa-facebook-square fa-lg"></i> Follow us on Facebook</a></li>
+                </ul>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <h5>Contacts</h5>
+                <dl class="contact-list">
+                    <dt>Phone:</dt>
+                    <dd><a href="tel:+1844799790"><i class="fas fa-phone"></i> Call 844-799-7900</a> 
+                        <br> <i class="fas fa-fax"></i> Fax 844-205-7353</dd>
+                </dl>
+                <dl class="contact-list">
+                    <dt>Address: <a href="https://g.page/TurquoiseTrucking?share" target="_blank" rel="noreferrer"><i class="fas fa-map-marker-alt"></i> (Directions)</a></dt>
+                    <dd>3330 Southgate Ct SW Ste&nbsp;222 
+                        <br> Cedar Rapids IA 52404</dd>
+                </dl>
+                <dl class="contact-list">
+                    <dt>Email:</dt>
+                    <dd><a href="mailto:contact@turquoisetruck.com"><i class="fas fa-envelope"></i> Send us a message</a></dd>
+                </dl>
+            </div>
+            <div class="col-md-4 col-xl-3">
+                <h5>Links</h5>
+                <ul class="nav-list">
+                    <li class="nav-home"><a href="index.html"><i class="fas fa-home"></i> Home</a></li>
+                    <li class="nav-about"><a href="about.html"><i class="fas fa-user-friends"></i> About Turquoise</a></li>
+                    <li class="nav-contact"><a href="contact.html"><i class="far fa-comment-dots"></i> Contact Us</a></li>
+                    <li class="nav-drivers"><a href="drivers.html"><i class="fas fa-truck-moving"></i> Drivers</a></li>
+                    <li class="nav-customers"><a href="customers.html"><i class="fas fa-warehouse"></i> Customers</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- secondary footer -->
+<div class="footer-dark small">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-xl-9">
+                <p>Made with <i class="fab fa-bootstrap"></i> Bootstrap, <i class="fab fa-font-awesome-flag"></i> Font Awesome, and <i class="fab fa-js-square"></i> jQuery.</p>
+            </div>
+            <div class="col-md-4 col-xl-3">
+                <p>Copyright &copy; Turquoise, LLC.</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php wp_footer(); ?>
+
+<!-- jquery required by bootstrap -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS 
+         full jQuery needed over slim for return to top arrow -->
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"
+        integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+        crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" 
+        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" 
+        crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" 
+        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" 
+        crossorigin="anonymous"></script>
+    <!-- local script -->
+    <script src="/static-assets/turquoise.js"></script>
+
+</body>
 </html>
