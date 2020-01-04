c6240e7429499c6ce9062051d16b505160aad330
turquoise-trucking <57842675+turquoise-trucking@users.noreply.github.com>
2020-01-04 16:32:18 -0600

    Last minute cleanup and move remove filter to function

--- functions.php
+++ functions.php
@@ -1,5 +1,7 @@
 <?php 
 
+  remove_filter ('the_content', 'wpautop');
+
     function themebs_enqueue_styles() {
 
         wp_enqueue_style( 'bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css' );
--- index.php
+++ index.php
@@ -1,7 +1,5 @@
 <?php get_header(); ?>
 
-<?php remove_filter ('the_content', 'wpautop'); ?>
-
 <?php if ( have_posts() ) : while ( have_posts() ) :   the_post(); ?>
   <?php the_content(); ?>
 <?php endwhile; else: ?>
