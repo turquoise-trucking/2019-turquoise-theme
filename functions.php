468f4a82778fc0c299a8f4133e42fbd951cd3d28
turquoise-trucking <57842675+turquoise-trucking@users.noreply.github.com>
2020-01-18 15:42:44 -0600

    Style consolidation & CDN enqueue
    Removed the standalone stylesheet and instead added those to the theme default style.css file. Functions now enqueue themes and scripts via CDN instead of standalone files.

--- functions.php
+++ functions.php
@@ -4,16 +4,14 @@
 
     function themebs_enqueue_styles() {
 
-        wp_enqueue_style( 'bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css' );
+        wp_enqueue_style( 'bootstrap', '//stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css' );
         wp_enqueue_style( 'core', get_template_directory_uri() . 'style.css' );
-        wp_enqueue_style( 'turquoise', get_template_directory_uri() . '/css/2019-turquoise.css' );
-      
       }
-      add_action( 'wp_enqueue_scripts', 'themebs_enqueue_styles');
+      add_action( 'wp_enqueue_styles', 'themebs_enqueue_styles');
       
       function themebs_enqueue_scripts() {
-        wp_enqueue_script( 'bootstrap', get_template_directory_uri() . '/js/bootstrap-4.1.3.min.js', array( 'jquery' ) );
-        wp_enqueue_script( 'popper', get_template_directory_uri() . '/js/popper-1.12.9.min.js');
+        wp_enqueue_script( 'bootstrap', '//stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js' );
+        wp_enqueue_script( 'popper', '//unpkg.com/@popperjs/core@2.0.0-rc.3' );
       }
       add_action( 'wp_enqueue_scripts', 'themebs_enqueue_scripts');
 ?>
