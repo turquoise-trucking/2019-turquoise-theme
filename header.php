aef28793c15009a002269e304580e9a87dc01baa
turquoise-trucking <57842675+turquoise-trucking@users.noreply.github.com>
2020-02-01 15:06:49 -0600

    Readying header.php for enqueing and fixed alt in footer

--- header.php
+++ header.php
@@ -1,31 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    
-    <title><?php wp_title(); ?></title>
-
-    <meta name="author" content="John Earl Hardesty">
-
-    <!-- bootstrap styles -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" 
-        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" 
-        crossorigin="anonymous">
-    <!-- font awesome -->
-    <script src="https://kit.fontawesome.com/74abbf3163.js" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css">
-    <!-- favicon -->
-    <link rel="icon" type="image/png" sizes="16x16" href="/static-assets/tt-icon-16px.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="/static-assets/tt-icon-32px.png">
-    <link rel="icon" type="image/png" sizes="64x64" href="/static-assets/tt-icon-64px.png">
-    <link rel="apple-touch-icon" type="image/png" href="/static-assets/tt-icon-apple.png">
-    <!-- local styles -->
-    <link rel="stylesheet" href="/static-assets/turquoise.css">
-
-    <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
-
-    <?php wp_head(); ?>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<title><?php wp_title(); ?></title>
+<meta name="author" content="John Earl Hardesty">
+<!-- favicon -->
+<link rel="icon" type="image/png" sizes="16x16" href="/static-assets/tt-icon-16px.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/static-assets/tt-icon-32px.png">
+<link rel="icon" type="image/png" sizes="64x64" href="/static-assets/tt-icon-64px.png">
+<link rel="apple-touch-icon" type="image/png" href="/static-assets/tt-icon-apple.png">
+<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+<!-- wp loop -->
+<?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
