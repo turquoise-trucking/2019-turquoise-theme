eec50ebbe4eaadcd91dfa1381392bf716fa5a98f
turquoise-trucking <57842675+turquoise-trucking@users.noreply.github.com>
2020-02-01 16:27:18 -0600

    Enqueueing works

--- functions.php
+++ functions.php
@@ -1,19 +1,18 @@
 <?php 
 
   remove_filter ('the_content', 'wpautop');
-
-    function themebs_enqueue_styles() {
-
-        wp_enqueue_style( 'bootstrap', '//stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css' );
-        wp_enqueue_style( 'fontawesome' , '//stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css' );
-        wp_enqueue_style( 'core', get_template_directory_uri() . 'style.css' );
-      }
-      add_action( 'wp_enqueue_styles', 'themebs_enqueue_styles');
       
       function themebs_enqueue_scripts() {
+        wp_enqueue_style( 'bootstrap', '//stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css' );
+        wp_enqueue_style( 'fontawesome', '//cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0/css/all.min.css' );
+        wp_enqueue_style( 'core', get_template_directory_uri() . '/style.css' );
+        
+        wp_enqueue_script( 'jquery' , '//code.jquery.com/jquery-3.4.1.slim.min.js' );
         wp_enqueue_script( 'bootstrap', '//stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js' );
         wp_enqueue_script( 'popper', '//unpkg.com/@popperjs/core@2.0.0-rc.3' );
         wp_enqueue_script( 'fontawesome', '//kit.fontawesome.com/74abbf3163.js' );
+        wp_enqueue_script( 'turquoise', get_template_directory_uri() . '/js/turquoise.js' );
+        
       }
       add_action( 'wp_enqueue_scripts', 'themebs_enqueue_scripts');
 ?>
---
+++ js/turquoise.js
@@ -0,0 +1,16 @@
+/* Turquoise scripts ========================================================== */
+/* 2019-12-07 John Earl Hardesty */
+
+
+  
+
+
+$( document ).ready(function() {
+    $(function () {
+        $('[data-toggle="popover"]').popover()
+      })
+    $('.popover-dismiss').popover({
+        trigger: 'focus'
+      })
+
+});
