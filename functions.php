ef8e4e40e9220e6e0f31427250b2fe90d0f82149
turquoise-trucking <57842675+turquoise-trucking@users.noreply.github.com>
2020-02-08 17:21:49 -0600

    1.2 Update

--- functions.php
+++ functions.php
@@ -8,8 +8,8 @@
         wp_enqueue_style( 'core', get_template_directory_uri() . '/style.css' );
         
         wp_enqueue_script( 'jquery' , '//code.jquery.com/jquery-3.4.1.slim.min.js' );
-        wp_enqueue_script( 'bootstrap', '//stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js' );
         wp_enqueue_script( 'popper', '//unpkg.com/@popperjs/core@2.0.0-rc.3' );
+		wp_enqueue_script( 'bootstrap', '//stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js' );
         wp_enqueue_script( 'fontawesome', '//kit.fontawesome.com/74abbf3163.js' );
         wp_enqueue_script( 'turquoise', get_template_directory_uri() . '/js/turquoise.js' );
         
--- header.php
+++ header.php
@@ -10,10 +10,9 @@
 <link rel="icon" type="image/png" sizes="32x32" href="/static-assets/tt-icon-32px.png">
 <link rel="icon" type="image/png" sizes="64x64" href="/static-assets/tt-icon-64px.png">
 <link rel="apple-touch-icon" type="image/png" href="/static-assets/tt-icon-apple.png">
-<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+
 <!-- wp loop -->
-<?php wp_head(); ?>
-</head>
+<?php wp_head(); ?></head>
 
 <body <?php body_class(); ?>>
 <!-- masthead -->
@@ -25,7 +24,7 @@
                     <a href="/"><img class="logo" src="/static-assets/turquoise-logo.png" alt="turquoise trucking logo"></a>
                 </div>
                 <div class="col-sm-6 align-middle">
-                    <div class="btn-group float-right" style="padding: 0.75em;">
+                    <div class="btn-group float-right masthead-btn">
                         <a class="btn btn-success" href="tel:+18447997900" role="button"><i class="fas fa-phone"></i>&nbsp;Call 844-799-7900</a>
                         <a class="btn btn-primary" href="mailto:contact@turquoisetruck.com" role="button"><i class="fas fa-envelope"></i>&nbsp;Email&nbsp;us</a>
                     </div>
