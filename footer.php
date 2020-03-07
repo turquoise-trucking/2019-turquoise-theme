5b4c9f8cbe05d845b9022a6631f1eae9b10c9adf
turquoise-trucking <57842675+turquoise-trucking@users.noreply.github.com>
2020-03-07 15:32:43 -0600

    Updated footer to add memberships
    Also minimalized elements.

--- footer.php
+++ footer.php
@@ -1,53 +1,68 @@
 <!-- primary footer -->
-<div class="section footer-classic footer context-light bg-turquoise">
-    <div class="container">
-        <div class="row row-30">
-            <div class="col-md-4 col-xl-5">
-                <div class="pr-xl-4"><a class="brand" href="/"><img class="brand-logo-light" src="/static-assets/turquoise-badge.png" alt="Turquoise Trucking" width="181" height="50"></a>
-                <p>With over 20 years of experience in the trucking business, we provide companies with safe, efficient, and reliable transportation solutions across the United States.</p>
-                <ul class="nav-list">
-                    <li class="nav-fb"><a href="https://www.facebook.com/TurquoiseTrucking/" target="_blank" rel="noreferrer" class="external-link"><i class="fab fa-facebook-square fa-lg"></i> Follow us on Facebook</a></li>
-					<li class="nav-fb"><a href="https://www.redcross.org/" target="_blank" rel="noreferrer" class="external-link"><img src="/static-assets/footer-red-cross.png" alt="American Red Cross logo" class="redcross"> We support the American Red Cross</a></li>
-                </ul>
+<footer>
+    <div class="section footer-classic footer context-light bg-turquoise">
+        <div class="container">
+            <div class="row row-30">
+                <div class="col-md-4 col-xl-5">
+                    <div class="pr-xl-4"><a class="brand" href="/"><img class="brand-logo-light" src="/static-assets/turquoise-badge.png" alt="Turquoise Trucking" width="181" height="50"></a>
+                        <p>With over 20 years of experience in the trucking business, we provide companies with safe, efficient, and reliable transportation solutions across the United States.</p>
+                        <ul class="nav-list">
+                            <li class="nav-fb"><a href="https://www.facebook.com/TurquoiseTrucking/" target="_blank" rel="noreferrer" class="external-link"><i class="fab fa-facebook-square fa-lg"></i> Follow us on Facebook</a></li>
+                            <li class="nav-fb"><a href="https://www.redcross.org/" target="_blank" rel="noreferrer" class="external-link"><img src="/static-assets/footer-red-cross.png" alt="" class="redcross"> We support the Red&nbsp;Cross</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <h3>Contacts</h3>
+                    <dl class="contact-list">
+                        <dt>Phone:</dt>
+                        <dd><a href="tel:+1844799790"><i class="fas fa-phone fa-lg"></i> 844-799-7900</a> </dd>
+                    </dl>
+                    <dl class="contact-list">
+                        <dt>Address:</dt>
+                        <dd><a class="external-link" href="https://g.page/TurquoiseTrucking?share" target="_blank" rel="noreferrer"><i class="fas fa-map-marker-alt fa-lg"></i> 3330 Southgate Ct SW Ste&nbsp;222 
+                            <br> Cedar Rapids IA 52404</a></dd>
+                    </dl>
+                    <dl class="contact-list">
+                        <dt>Email:</dt>
+                        <dd><a href="mailto:contact@turquoisetruck.com"><i class="fas fa-envelope fa-lg"></i> Send us a message</a></dd>
+                    </dl>
+                </div>
+                <div class="col-md-4 col-xl-3">
+                    <h3>Links</h3>
+                    <ul class="nav-list">
+                        <li class="nav-home"><a href="/"><i class="fas fa-home"></i> Home</a></li>
+                        <li class="nav-about"><a href="/about/"><i class="fas fa-user-friends"></i> About Turquoise</a></li>
+                        <li class="nav-contact"><a href="/contact/"><i class="far fa-comment-dots"></i> Contact Us</a></li>
+                        <li class="nav-drivers"><a href="/drivers/"><i class="fas fa-truck-moving"></i> Drivers</a></li>
+                        <li class="nav-customers"><a href="/customers/"><i class="fas fa-warehouse"></i> Customers</a></li>
+                    </ul>
                 </div>
             </div>
-            <div class="col-md-4">
-                <h5>Contacts</h5>
-                <dl class="contact-list">
-                    <dt>Phone:</dt>
-                    <dd><a href="tel:+1844799790"><i class="fas fa-phone"></i> Call 844-799-7900</a> 
-                        <br> <i class="fas fa-fax"></i> Fax 844-205-7353</dd>
-                </dl>
-                <dl class="contact-list">
-                    <dt>Address: <a href="https://g.page/TurquoiseTrucking?share" target="_blank" rel="noreferrer"><i class="fas fa-map-marker-alt"></i> (Directions)</a></dt>
-                    <dd>3330 Southgate Ct SW Ste&nbsp;222 
-                        <br> Cedar Rapids IA 52404</dd>
-                </dl>
-                <dl class="contact-list">
-                    <dt>Email:</dt>
-                    <dd><a href="mailto:contact@turquoisetruck.com"><i class="fas fa-envelope"></i> Send us a message</a></dd>
-                </dl>
-            </div>
-            <div class="col-md-4 col-xl-3">
-                <h5>Links</h5>
-                <ul class="nav-list">
-                    <li class="nav-home"><a href="/"><i class="fas fa-home"></i> Home</a></li>
-                    <li class="nav-about"><a href="/about/"><i class="fas fa-user-friends"></i> About Turquoise</a></li>
-                    <li class="nav-contact"><a href="/contact/"><i class="far fa-comment-dots"></i> Contact Us</a></li>
-                    <li class="nav-drivers"><a href="/drivers/"><i class="fas fa-truck-moving"></i> Drivers</a></li>
-                    <li class="nav-customers"><a href="/customers/"><i class="fas fa-warehouse"></i> Customers</a></li>
-                </ul>
+            <div class="row">
+                <div class="col">
+                    <h3>Memberships & Causes <i title="Following links open external sites." class="fas fa-external-link-alt"></i></h3>
+                </div>
+            </div> 
+            <div class="row membership">
+                <div class="col">
+                    <ul>
+                        <li><a href="https://www.cvsa.org/"><img alt="CVSA, Commercial Vehicle Safety Alliance." src="/static-assets/footer-CVSA.png"></a></li>
+                        <li><a href="https://www.nastc.com/"><img alt="NASTC, National Association of Small Trucking Companies." src="/static-assets/footer-NASTC.png"></a></li>
+                        <li><a href="http://www.iowamotortruck.com/"><img alt="IMTA, Iowa Motor Trucking Association." src="/static-assets/footer-IMTA.png"></a></li>
+                        <li><a href="https://www.soiowa.org/"><img alt="Special Olympics Iowa." src="/static-assets/footer-SO.svg"></a></li>
+                    </ul>
+                </div>
             </div>
         </div>
     </div>
-</div>
-
+</footer>
 <!-- secondary footer -->
 <div class="footer-dark small">
     <div class="container">
         <div class="row">
             <div class="col-md-8 col-xl-9">
-                <p>Made with <i class="fab fa-bootstrap"></i> Bootstrap, <i class="fab fa-font-awesome-flag"></i> Font Awesome, and <i class="fab fa-js-square"></i> jQuery.</p>
+                <p>Made with <a class="external-link" href="https://getbootstrap.com/"><i class="fab fa-bootstrap"></i> Bootstrap</a> and <a class="external-link" href="https://fontawesome.com/"><i class="fab fa-font-awesome-flag"></i> Font Awesome</a> in <a class="external-link" href="http://www.cedar-rapids.org/">Cedar Rapids</a>.</p>
             </div>
             <div class="col-md-4 col-xl-3">
                 <p>Copyright &copy; Turquoise, LLC.</p>
