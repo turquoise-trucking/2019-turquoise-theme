8040701b4d851f0f5ee3447c082f3001bfcfbfa3
turquoise-trucking <57842675+turquoise-trucking@users.noreply.github.com>
2020-03-14 17:46:32 -0500

    Create oops.php

---
+++ oops.php
@@ -0,0 +1,7 @@
+<!-- error message -->
+<div class="container-fluid text-center">
+    <h1 class="display-4">Oops. Something went wrong.</h1>
+    <p class="lead">We couldn't find what you were looking for.</p>
+    <img class="mx-auto img-fluid" src="/static-assets/oops-question-mark.jpg" alt="Stylized person leaning on question mark.">
+    <p class="attribution">Image by <a href="https://pixabay.com/users/Peggy_Marco-1553824/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=3978394">Peggy and Marco Lachmann-Anke</a> from <a href="https://pixabay.com/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=3978394">Pixabay</a></p>
+</div>
